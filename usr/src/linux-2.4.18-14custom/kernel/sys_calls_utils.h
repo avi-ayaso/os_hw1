f9afcfb1969d415c653a421f3d13d75113c669a3
mishuta1990 <34060524+mishuta1990@users.noreply.github.com>
2018-11-17 12:51:28 +0200

    Update sys_calls_utils.h

--- usr/src/linux-2.4.18-14custom/kernel/sys_calls_utils.h
+++ usr/src/linux-2.4.18-14custom/kernel/sys_calls_utils.h
@@ -5,18 +5,6 @@
 #include <linux/slab.h>
 
 
-// output will be in the given pointer
-int copy_forbidden_activity_list_to_user(task_t * process , forbidden_activity_info * ret_val , int size) {
-    unsigned long status = copy_to_user((void *)ret_val, (void *)(process->_log), sizeof(forbidden_activity_info)*size);
-    if (status > 0) return -22;
-    int index=0;
-    int i=0;
-    for (i=size;i<process->num_of_violations;++i,++index){
-        process->_log[index] = process->_log[i];
-    }
-    process->num_of_violations = index;
-    return 0;
-}
 
 int add_forbidden_activity_to_log(task_t * _current , int _syscall_req_level) {
     int num_of_violations = _current->num_of_violations;
